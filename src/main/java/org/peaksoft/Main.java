382241c389abf7de6849cb6eb3f8ded65c264897
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-06-04 22:00:11 +0300

    first commit

--- src/main/java/org/peaksoft/Main.java
+++ src/main/java/org/peaksoft/Main.java
@@ -7,12 +7,15 @@ public class Main {
         // реализуйте алгоритм здесь
         //Util.connection();
         UserDaoJdbcImpl daoJdbc=new UserDaoJdbcImpl();
-        //daoJdbc.createUsersTable();
+//        daoJdbc.createUsersTable();
+//
 //        daoJdbc.saveUser("Karim","Benzema", (byte) 34);
 //        daoJdbc.saveUser("Luka","Modric", (byte) 36);
 //        daoJdbc.saveUser("Toni","Kroos",(byte) 32);
 //        daoJdbc.saveUser("Federico","Valverde", (byte) 23);
 //        daoJdbc.saveUser("Eduardo","Camavinga", (byte) 19);
+
         System.out.println(daoJdbc.getAllUsers());
+
     }
 }
