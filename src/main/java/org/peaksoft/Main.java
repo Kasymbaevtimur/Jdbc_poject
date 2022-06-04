c4472f08d5d0a7483ed6048ad9e3c832acfbbd11
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-06-04 23:17:48 +0300

    third commit

--- src/main/java/org/peaksoft/Main.java
+++ src/main/java/org/peaksoft/Main.java
@@ -4,18 +4,21 @@ import org.peaksoft.dao.UserDaoJdbcImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        //Util.connection();
-        UserDaoJdbcImpl daoJdbc=new UserDaoJdbcImpl();
-//        daoJdbc.createUsersTable();
-//
-//        daoJdbc.saveUser("Karim","Benzema", (byte) 34);
-//        daoJdbc.saveUser("Luka","Modric", (byte) 36);
-//        daoJdbc.saveUser("Toni","Kroos",(byte) 32);
-//        daoJdbc.saveUser("Federico","Valverde", (byte) 23);
-//        daoJdbc.saveUser("Eduardo","Camavinga", (byte) 19);
+
+
+        UserDaoJdbcImpl daoJdbc = new UserDaoJdbcImpl();
+
+        daoJdbc.createUsersTable();
+
+        daoJdbc.saveUser("Karim", "Benzema", (byte) 34);
+        daoJdbc.saveUser("Luka", "Modric", (byte) 36);
+        daoJdbc.saveUser("Toni", "Kroos", (byte) 32);
+        daoJdbc.saveUser("Federico", "Valverde", (byte) 23);
+        daoJdbc.saveUser("Eduardo", "Camavinga", (byte) 19);
 
         System.out.println(daoJdbc.getAllUsers());
+        daoJdbc.cleanUsersTable();
+        daoJdbc.dropUsersTable();
 
     }
 }
